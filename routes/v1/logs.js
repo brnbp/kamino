767b7abbc80d9a9f2e0557e5ff104231ecedb561
brnbp <bruno9pereira@gmail.com>
2017-01-29 01:34:49 -0200

    change responser to responder

--- routes/v1/logs.js
+++ routes/v1/logs.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const responser = require('../response')()
+const responder = require('../response')()
 
 const LogModel = require('../../models/log')
 let LogsController = require('../../controllers/logs')
 LogsController = new LogsController(new LogModel)
 
 router.get('/latests', function(req, res) {
-  responser(res, LogsController.latests())
+  responder(res, LogsController.latests())
 });
 
 router.get('/:id', function(req, res) {
-  responser(res, LogsController.one(req.params.id))
+  responder(res, LogsController.one(req.params.id))
 });
 
 module.exports = router;
