4f5ca42a4e14d1bab6e19ac51bfdbd68d89503bc
brnbp <bruno9pereira@gmail.com>
2017-01-29 02:24:33 -0200

    update function syntax to class

--- controllers/response.js
+++
@@ -1,7 +0,0 @@
-const response = (data, statusCode) => ({data: data, statusCode: statusCode})
-
-module.exports = {
-  success: (data = {}, statusCode = 200) => response({data:data}, statusCode),
-  error: (message = '', statusCode = 400) => response({error: message}, statusCode),
-  notFound: (message = 'not found') => response({error: message}, 404)
-}
